bd4126b736f20b067ef1cfadc0a18cda03079143
TayoAdepetu <copyafrica93@gmail.com>
2023-03-06 11:10:18 -0800

    commit nowww

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Models\User;
 use Validator;
 
@@ -14,27 +15,27 @@ class LoginController extends Controller
 {
     public function loginUser(Request $request)
     {
-        try{
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|string'
-        ]);
+        try {
+            $credentials = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required|string'
+            ]);
 
-        if(!$credentials) {
-            return response()->json(['success'=> false, 'error'=> $credentials->messages()], 401);
-        }
+            if (!$credentials) {
+                return response()->json(['success' => false, 'error' => $credentials->messages()], 401);
+            }
 
-        if ($token = Auth::attempt($request->only('email', 'password'))) {
-            return $this->respondWithToken($token);
-        }
+            if ($token = Auth::attempt($request->only('email', 'password'))) {
+                return $this->respondWithToken($token);
+            }
 
-        /* throw ValidationException::withMessages([
+            /* throw ValidationException::withMessages([
             'email' => ['The provided credentials are incorrect.']
-        ]); */
-    }catch(JWTException $exception){
-      return response()->json($exception);
+            ]); */
+        } catch (JWTException $exception) {
+            return response()->json($exception);
+        }
     }
-
 
     /**
      * Get the authenticated User.
