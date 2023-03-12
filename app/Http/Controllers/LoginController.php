b81cef9f3592f982696e3bef864c2beade6c1ba7
TayoAdepetu <copyafrica93@gmail.com>
2023-03-12 09:32:11 -0700

    commit user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,7 +34,6 @@ class LoginController extends Controller
                 return response()->json([
                 'access_token' => strval($token),
                 'token_type' => 'bearer',
-                'user' => $user,
                 //'expires_in' => $this->guard()->factory()->getTTL() * 60 * 60 * 3 // to expire in 3 hours 
                 "expires_in" => Auth::factory()->getTTL() * 60 * 60 * 3 // to expire in 3 hours
         ]);
