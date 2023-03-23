0f12040de28976859b9fa20e71b8620b6e4782b2
TayoAdepetu <copyafrica93@gmail.com>
2023-03-23 06:01:10 -0700

    blogimages

--- database/migrations/2023_03_15_144139_create_blogimages_table.php
+++ database/migrations/2023_03_15_144139_create_blogimages_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('afrimages', function (Blueprint $table) {
+        Schema::create('blogimages', function (Blueprint $table) {
             $table->id();
             $table->string('image_path')->unique();
             $table->integer('user_id');
